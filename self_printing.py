30303e18f3455fd10f03cfe80f4775e4fd012156
Chris Weller <chriskweller@gmail.com>
2015-05-17 16:38:04 -0700

    Git this stuff up2date.

---
+++ self_printing.py
@@ -0,0 +1,24 @@
+idxx= 0 ##.
+sc = open("script.txt").readlines()
+print ''.join(open(__file__).readlines()[3:])
+
+#Test.......
+f = open(__file__)
+s = f.readlines()
+
+r = s
+
+f.close()
+
+for idx,x in enumerate(s):
+    if "##" in x and x[-2] == '.':
+        r[idx] = ''.join([" " + str((int(m)+1)%len(sc)) + 
+          " " if m.isdigit() else m for m in x.split()] + ["\n"])
+    elif "#" in x and (x[-2] == '.' or x[-2] == '?'):
+        r[idx] = sc[idxx]
+
+#print ''.join(r)
+f = open(__file__,'w')
+f.write(''.join(r))
+f.close()
+
