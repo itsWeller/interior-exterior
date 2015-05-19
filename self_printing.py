f01c30afaf9c1f4f47a64e6779ce04712a67061c
Chris Weller <chriskweller@gmail.com>
2015-05-18 20:21:16 -0700

    Alright, I think I'm done with this shit.

--- self_printing.py
+++ self_printing.py
@@ -1,14 +1,11 @@
-# -*- coding: utf-8 -*-
 from tabulate import tabulate; import random
 idxx = 0 ##.
 sc = open(".script.txt").readlines()
-msg="Stop. It's already too late"*25
-if idxx>25:
-    aas=unicode("\n\n\t\tG̮͔̯͓̻̣͖̠͉̳̯͕͈͙̤̱̓̿̈ͬͨ͗ͤ͒ͪ̈́̇̀̃̅̈͐̚ ̰̺͉̭̳̝͎̙̠̞̻͑̔̈́̄͒̚ o̖̜͔̪̳̺͚̠̩̦͍͖̖͚͎̍̎͊̄̿̒̊͑̎͐ͣ̌ͭ͑ ͍̫̺̒͊̈ͨ̇́̚ o̰̰͓̮͇̮̹̖̜̼͔̦̪ͭ̓̅͊ͭͮ̊̅̋̇́͌̎͑͐̒ ̖̩͈̱̤̥̗̭̤͖̜̻̼̯͆̒ͧ̎ͧ͛̇̓̒ ḋ̫͍̱͇̙͙̳̖̰͉̜̤̳̘̥͕̤ͩ͗ͪ̈́͂ͧ̈̏͑̌͊̆ͫͅ ̦͈̘͙̩̗̆ͦ̊ͧͮ̀̄ͦ̽ͧͤ̄̏ͪ̂̚ b̬͙͓̙̬̳͇̰̖͇̻͕ͧ̾͑̑ͦ̑̈͂ͯ͊ ̜̱̰̰͉̱͔̜̹͎̞̙͓̺͖̞̹̿̿ͤ͗̏͋ͅ y̘̺̙̣̰̮̮̰̟͓̦͉̼̺̠̱͍͓ͤ͒ͭ̋ͬ͂͋͐̎̇̈́̍̽ ̳̳̫̭͙̬̦ͮͣͤ͗̊ͨ e̹̫̻̱̘͈̭͔͉̎͛͗͑̌ͤ͗̍̋̂̇\n\n",'utf-8')
-    print aas
-    exit();
-print ''.join(open(__file__).readlines()[10:])
-
+#msg="Stop. It's already too late"*25
+msg="Sorry.Time's up.Say goodbye"*25
+if idxx>28:
+    f = open(__file__,'w'); f.write("'''\n# Don't worry, you'll be seeing me again soon\n'''\nprint('Goodbye.')\n"); f.close()
+print ''.join(open(__file__).readlines()[9:])
 f = open(__file__);s = f.readlines();f.close();r = s
 
 
@@ -24,7 +21,7 @@ for idx,x in enumerate(s):
 
 
 '''
-    # There's probably a better way to do this, but w/e.
+    # There's probably a better way to do this, but w/e. I'm lazy.
 '''
 
 tdpg=[['' for _ in xrange(27)] for _ in xrange(5)]
@@ -32,9 +29,8 @@ for x in xrange(27*5):tdpg[x/27][x%27] = str(random.randrange(0,10))
 
 for _ in xrange((idxx)*7): 
     na=random.randrange(0,27*5);tdpg[na/27][na%27]=msg[na%len(sc)]
-if idxx > 22:
+if idxx > 26:
     for x in xrange(5*27): tdpg[x/27][x%27] = msg[x]
 
-#print ''.join(r)
 print tabulate(tdpg);f = open(__file__,'w'); f.write(''.join(r)); f.close()
 
